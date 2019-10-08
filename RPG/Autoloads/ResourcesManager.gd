fddf51e31ec6ba73da605a75d46d317beb6140e4
Rido <daniridao@hotmail.com>
2019-10-08 21:38:57 +0100

    Loading games with their proper assets from server

---
+++ RPG/Autoloads/ResourcesManager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# warning-ignore:unused_class_variable
+var characters = [
+	{
+		portrait = "res://Assets/Graphics/Portraits/Dissicon_ff8_Squ3.png"
+	},
+	{
+		portrait = "res://Assets/Graphics/Portraits/Dissicon_ff8_Lag4.png"
+	}
+]
