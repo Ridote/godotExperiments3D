9364c200d988501f08c22d381ef81c5d3801e8e0
Rido <daniridao@hotmail.com>
2019-10-11 22:48:42 +0100

    ultimos cambios

--- RPG/Autoloads/ResourcesManager.gd
+++ RPG/Autoloads/ResourcesManager.gd
@@ -1,11 +1,18 @@
+# warning-ignore-all:unused_class_variable
+
 extends Node
 
-# warning-ignore:unused_class_variable
 var characters = [
 	{
-		portrait = "res://Assets/Graphics/Portraits/Dissicon_ff8_Squ3.png"
+		portrait = "res://Assets/Graphics/Portraits/Dissicon_ff8_Squ3.png",
 	},
 	{
 		portrait = "res://Assets/Graphics/Portraits/Dissicon_ff8_Lag4.png"
+	}
+]
+
+var maps = [
+	{
+		name = "La casa del Jairo"
 	}
 ]
